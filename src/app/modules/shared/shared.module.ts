7dd00220c12616a8d0e6e1f7bec4c78e3e9368fd
Lean0111 <leandroperez2147@gmail.com>
2024-10-03 10:51:41 -0300

    modificaciones en el diseño del footer y creacion del componente acercade

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+//material
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { CardComponent } from './components/card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    CardComponent,
+    FooterComponent,
+    AcercadeComponent,
+    
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatMenuModule
+    
+  ],
+  exports:[
+    NavbarComponent,
+    CardComponent,
+    FooterComponent,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+
+  ]
+})
+export class SharedModule { }
