4ac56600ee1e9c8b56757503c0daa987a503f00e
javierjr30789 <javierjr30789@gmail.com>
2024-10-30 11:15:06 -0300

    arreglando estilos de la pagina

---
+++ src/app/filtro-nombre.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Producto } from './models/producto'; // Ajusta la ruta según tu estructura de carpetas
+
+@Pipe({
+  name: 'filtroNombre'
+})
+export class FiltroNombrePipe implements PipeTransform {
+  transform(productos: Producto[], filtro: string): Producto[] {
+    if (!productos || !filtro) {
+      return productos;
+    }
+    return productos.filter(producto =>
+      producto.nombre.toLowerCase().includes(filtro.toLowerCase())
+    );
+  }
+}
