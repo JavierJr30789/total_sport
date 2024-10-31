64cf90761675c17b7d0ba78135995a9cf5fd6d91
javierjr30789 <javierjr30789@gmail.com>
2024-10-31 10:21:56 -0300

    arreglando el carrusel bugeado

--- src/app/modules/inicio/pages/inicio/inicio.component.ts
+++ src/app/modules/inicio/pages/inicio/inicio.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/modules/admin/services/crud.service';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+  productosPorCategoria: Producto[] = [];
 
+  constructor(private crudService: CrudService) {}
+
+  ngOnInit(): void {
+    this.crudService.obtenerProducto().subscribe((productos) => {
+      // Filtramos los productos para obtener solo uno por cada categoría
+      const categoriasUnicas = new Set();
+      this.productosPorCategoria = productos.filter((producto) => {
+        if (!categoriasUnicas.has(producto.categoria)) {
+          categoriasUnicas.add(producto.categoria);
+          return true;
+        }
+        return false;
+      });
+    });
+  }
 }
